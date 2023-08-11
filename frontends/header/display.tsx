93c11249138fe8ea7448f2e9e20595a3a8126c0d
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-08-11 18:09:55 +0300

    Begin working header and footer

--- frontends/header/display.tsx
+++ frontends/header/display.tsx
@@ -1,10 +1,16 @@
 import { Cemjsx } from "cemjs-all"
+import user from '@svg/icons/user.svg'
+import chat from '@svg/icons/chat.svg'
 
 export const display = function () {
 	return (
 		<header class="header">
-			<div class="header__container">
-				123
+			<div>
+				<img class="icon" src={chat} alt="Вход или регистрация" />
+			</div>
+			<div>
+				<span>Вход</span>
+				<img class="icon" src={user} alt="Вход или регистрация" />
 			</div>
 		</header>
 	)
