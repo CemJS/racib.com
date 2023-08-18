0e170fdd5309b6d593b35d9f20cb88b334a4bb48
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-08-18 16:37:13 +0300

    Add particles microfrontend on the main page

--- frontends/particles/display.tsx
+++ frontends/particles/display.tsx
@@ -1,9 +1,15 @@
 import { Cemjsx } from "cemjs-all"
 
 export const display = function () {
+  // console.log('=4fe088=', this.Ref.canvas)
   return (
-    <div class="particles_wrap">
-      <canvas id="canvas"></canvas>
+    <div class="particles_wrap"
+      ref="wrapCanvas"
+      onclick={() => {
+        this.fn("resize")
+      }}
+    >
+      <canvas id="canvas" ref="canvas"></canvas>
     </div>
   )
 }
---
+++ frontends/particles/listener.ts
@@ -0,0 +1,13 @@
+const listener = {
+    "start": function (data, name) {
+        // console.log('=e60c81=', data, name, this)
+    },
+    "finish": function (data, name, t) {
+        // console.log('=e60c81=', data, name, t, this)
+        // this.fn("resize");
+        // this.init();
+        // console.log('=80971f=', this.Ref.canvas);
+    }
+}
+
+export { listener }
