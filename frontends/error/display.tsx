c26f7a2207b3b5b0f3be792a40a600eae7d021fd
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-08-28 12:54:07 +0300

    Add favicon icon and page error 404

--- frontends/error/display.tsx
+++ frontends/error/display.tsx
@@ -3,7 +3,28 @@ import { Cemjsx } from "cemjs-all"
 export const display = function () {
   return (
     <div class="main_wrap">
+      <main
+        class={["main", this.Variable.openSidebar ? null : "main_close"]}
+      >
+        <div class="wrapper">
 
+          <section class="error_wrap">
+            <div class="error">
+              <p class="error_text">Страница не найдена</p>
+              <p class="error_main">Error 4
+                <span class="error_image"></span>
+                4</p>
+
+              <p class="error_text">Вернитесь на главную страницу</p>
+              <a href="/" class="btn_link btn_link_dark">
+                Главная
+              </a>
+
+            </div>
+          </section>
+
+        </div>
+      </main>
     </div>
   )
 }
