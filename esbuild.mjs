9106c17d53c4a6ae00dbedffa1b7bc55f8221530
Betarost <betarost@yandex.ru>
2023-09-07 08:49:50 +0300

    .

--- esbuild.mjs
+++ esbuild.mjs
@@ -218,7 +218,7 @@ const start = async function () {
                 }
             })
 
-            if (!haveChange && req.url !== "/esbuild" && !req.url.startsWith("/assets") && !req.url.startsWith("/docs")) {
+            if (!haveChange && req.url !== "/esbuild" && !req.url.startsWith("/assets") && !req.url.startsWith("/docs") && !req.url.startsWith("/contents")) {
                 req.url = "/"
             }
             proxy.web(req, res, { target: `http://${options.hostname}:${options.port}`, changeOrigin: true });
