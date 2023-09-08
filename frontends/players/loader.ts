db5981b1bb18c1cc9611ee88924475bbc9d69e0d
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-09-08 13:30:05 +0300

    processing players

--- frontends/players/loader.ts
+++ frontends/players/loader.ts
@@ -1,4 +1,11 @@
+import allUsers from '@json/allUsers'
+
 export const loader = function () {
+
+  if (this.Variable.DataUrl[2]) {
+    this.Static.record = allUsers[this.Variable.DataUrl[2]]
+  }
+
   this.Static.activeTab = '';
   this.Static.videoReady = false;
 }
