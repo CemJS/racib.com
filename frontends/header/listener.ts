dd5f52c21cc2988dfb180aac28d46bb1c371bf16
Betarost <betarost@yandex.ru>
2023-09-14 14:57:49 +0300

    .

--- frontends/header/listener.ts
+++ frontends/header/listener.ts
@@ -1,8 +1,8 @@
 export const clickAny = function (e) {
-    if (!this.Ref.sidebar.contains(e.target) && !this.Ref.burger.contains(e.target)) {
+    if (!this.Ref.sidebar.contains(e.target) && !this.Ref.burger.contains(e.target) && this.Variable.openSidebar) {
         this.Ref.sidebar.classList.add('sidebar_close');
         this.Variable.openSidebar = false;
+        this.Fn.initAll();
     }
-    this.Fn.initAll();
     return
 }
