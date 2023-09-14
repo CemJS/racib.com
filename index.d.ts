d361d5f7e1ac480c178b1a15429fb7e0afeeacaa
Betarost <betarost@yandex.ru>
2023-09-14 08:50:28 +0300

    .

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,17 @@ interface CemJS {
     version: string
     port: number
 }
+
+interface Frontend {
+    loader: Function,
+    display: Function,
+    listener: any,
+    func: any
+}
+
+interface Configs {
+    cemjs: any
+    pages: any
+    frontends: any
+    services: any
+}
