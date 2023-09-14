979b8d946eb3cec597a88ef68a07e2c903dd7d7b
Betarost <betarost@yandex.ru>
2023-09-14 14:15:42 +0300

    .

--- frontends/players/index.ts
+++ frontends/players/index.ts
@@ -1,11 +1,12 @@
-import { Micro } from "cemjs-all"
+import { Frontend } from "cemjs-all"
 import { loader } from "./loader"
 import { display } from "./display"
-import { fn } from "./functions"
+import * as listener from "./listener"
+import * as func from "./functions"
 
-export const frontend: Micro = {
-    name: "MicroTest",
+export const frontend: Frontend = {
     loader,
     display,
-    fn
+    listener,
+    func
 }
