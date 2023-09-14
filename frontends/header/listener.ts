60a4fcb4b7f19da336909620d342304af8b8a90e
Betarost <betarost@yandex.ru>
2023-09-14 14:52:31 +0300

    .

--- frontends/header/listener.ts
+++ frontends/header/listener.ts
@@ -1,11 +1,8 @@
-const listener = {
-    "clickAny": function (e) {
-        if (!this.Ref.sidebar.contains(e.target) && !this.Ref.burger.contains(e.target)) {
-            this.Ref.sidebar.classList.add('sidebar_close');
-            this.Variable.openSidebar = false;
-        }
-        this.Fn.initAll();
+export const clickAny = function (e) {
+    if (!this.Ref.sidebar.contains(e.target) && !this.Ref.burger.contains(e.target)) {
+        this.Ref.sidebar.classList.add('sidebar_close');
+        this.Variable.openSidebar = false;
     }
+    this.Fn.initAll();
+    return
 }
-
-export { listener }
