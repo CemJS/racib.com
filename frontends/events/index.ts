a32665abb2ffc43eb49ee897b26cb2c4a479b2fe
Betarost <betarost@yandex.ru>
2023-09-14 14:23:38 +0300

    .

--- frontends/events/index.ts
+++ frontends/events/index.ts
@@ -1,13 +1,12 @@
-import { Micro } from "cemjs-all"
+import { Frontend } from "cemjs-all"
 import { loader } from "./loader"
 import { display } from "./display"
-import { fn } from "./functions"
-import { listener } from "./listener"
+import * as listener from "./listener"
+import * as func from "./functions"
 
-export const frontend: Micro = {
-    name: "MicroTest",
+export const frontend: Frontend = {
     loader,
     display,
-    fn,
-    listener
+    listener,
+    func
 }
--- frontends/news/functions.ts
+++ frontends/news/functions.ts
@@ -1,25 +1,16 @@
-const fn = {
-    // "calendar": function () {
-
-    // }
-    "isLeapYear": function (year) {
-        return (
-            (year % 4 == 0 && year % 100 != 0 && year % 400 != 0) ||
-            (year % 100 == 0 && year % 400 == 0)
-        );
-    },
-    "getFebDay": function (year) {
-        if (this.fn("isLeapYear", year)) {
-            console.log(29);
-        } else {
-            console.log(28);
-        }
-
-        return this.fn("isLeapYear", year) ? 29 : 28;
-    },
-    "generatecalendar": function (month, year) {
-
-    }
+export const isLeapYear = function (year) {
+    return (
+        (year % 4 == 0 && year % 100 != 0 && year % 400 != 0) ||
+        (year % 100 == 0 && year % 400 == 0)
+    );
 }
 
-export { fn }
+export const getFebDay = function (year) {
+    if (this.fn("isLeapYear", year)) {
+        console.log(29);
+    } else {
+        console.log(28);
+    }
+
+    return this.fn("isLeapYear", year) ? 29 : 28;
+}
