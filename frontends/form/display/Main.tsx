e0e5c5a3447a97f27da9b76ae8283ff80209cc94
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-09-21 18:17:58 +0300

    form for join racib

--- frontends/form/display/Main.tsx
+++ frontends/form/display/Main.tsx
@@ -13,6 +13,10 @@ export default function () {
                 <span class="back_title">Как вступить?</span>
             </a>
 
+            <div class="form_appeal_wrap">
+                <p class="form_appeal">Уважаемые господа, благодарим вас за интерес к вопросам криптоиндустрии и желание стать участником Ассоциации РАКИБ. Чтобы стать полноправным участником, пожалуйста, заполните приведенные ниже поля и отправьте нам для дальнейшего взаимодействия. В течение нескольких дней после получения вашей заявки с вами свяжется наш сотрудник для завершения процесса регистрации.</p>
+            </div>
+
             <section class="form block_default">
                 <h1 class="join_title form_titleMain">Вступление в члены РАКИБ</h1>
                 <p class="form_subtitle">Прошу Вас принять меня в члены/участники «Ассоциации разработчиков и
@@ -21,11 +25,10 @@ export default function () {
 
                 <div class="form_choose">
                     <div
-                        class={["option",
-                            this.Static.face == 'Физическое лицо' ? "option_active" : null]}
+                        class={["option", this.Static.company ? null : "option_active"]}
                         for="physical"
                         onclick={() => {
-                            this.Static.face = 'Физическое лицо';
+                            this.Static.company = !this.Static.company;
                             this.init()
                         }}
                     >
@@ -33,11 +36,10 @@ export default function () {
                         <span>Физическое лицо</span>
                     </div>
                     <div
-                        class={["option",
-                            this.Static.face == 'Юридическое лицо' ? "option_active" : null]}
+                        class={["option", this.Static.company ? "option_active" : null]}
                         for="legal"
                         onclick={() => {
-                            this.Static.face = 'Юридическое лицо';
+                            this.Static.company = !this.Static.company;
                             this.init()
                         }}
                     >
@@ -49,7 +51,7 @@ export default function () {
                 <h3 class="join_title form_title">ФИО</h3>
                 <div class="form_block">
                     <div class="input">
-                        {
+                        {/* {
                             this.Static.lastName ?
                                 <span
                                     class="input_icon"
@@ -61,7 +63,7 @@ export default function () {
                                 >
                                     <img src={cancel} alt="Очистить поле ввода" />
                                 </span> : null
-                        }
+                        } */}
                         <input
                             class="input_field"
                             type="text"
@@ -72,7 +74,6 @@ export default function () {
                                 if (this.Ref.lastNameInput.value.length > 0) {
                                     this.Ref.lastNameLabel.classList.add('input_label_valid');
                                     this.Static.lastName = this.Ref.lastNameInput.value;
-                                    console.log('=7cf451=', this.Ref.lastNameInput.value)
                                 }
                                 if (this.Ref.lastNameInput.value.length === 0) {
                                     this.Ref.lastNameLabel.classList.remove('input_label_valid');
@@ -82,7 +83,6 @@ export default function () {
                             }}
                             oninput={() => {
                                 this.Static.lastName = this.Ref.lastNameInput.value;
-                                console.log('=7cf451=', this.Ref.lastNameInput.value)
                                 this.init();
                             }}
                         />
@@ -99,10 +99,16 @@ export default function () {
                             onchange={() => {
                                 if (this.Ref.firstNameInput.value.length > 0) {
                                     this.Ref.firstNameLabel.classList.add('input_label_valid');
+                                    this.Static.firstName = this.Ref.firstNameInput.value
                                 }
                                 if (this.Ref.firstNameInput.value.length === 0) {
                                     this.Ref.firstNameLabel.classList.remove('input_label_valid');
                                 }
+                                this.init()
+                            }}
+                            oninput={() => {
+                                this.Static.firstName = this.Ref.firstNameInput.value;
+                                this.init();
                             }}
                         />
                         <label class="input_label" for="firstName" ref="firstNameLabel">Имя</label>
@@ -118,10 +124,16 @@ export default function () {
                             onchange={() => {
                                 if (this.Ref.middleNameInput.value.length > 0) {
                                     this.Ref.middleNameLabel.classList.add('input_label_valid');
+                                    this.Static.middleName = this.Ref.middleNameInput.value
                                 }
                                 if (this.Ref.middleNameInput.value.length === 0) {
                                     this.Ref.middleNameLabel.classList.remove('input_label_valid');
                                 }
+                                this.init()
+                            }}
+                            oninput={() => {
+                                this.Static.middleName = this.Ref.middleNameInput.value
+                                this.init();
                             }}
                         />
                         <label class="input_label" for="middleName" ref="middleNameLabel">Отчество</label>
@@ -139,7 +151,145 @@ export default function () {
                         </div>
                     </div> */}
                 </div>
+
+                <h3 class="join_title form_title">Контактные данные</h3>
+                <div class="form_block">
+                    <div class="input">
+                        <input
+                            class="input_field"
+                            type="tel"
+                            name="tel"
+                            autocomplete="off"
+                            id="phone"
+                            pattern="+7[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}"
+                            ref="phoneInput"
+                            onchange={() => {
+                                if (this.Ref.phoneInput.value.length > 0) {
+                                    this.Ref.phoneLabel.classList.add('input_label_valid');
+                                    this.Static.phone = this.Ref.phoneInput.value;
+                                }
+                                if (this.Ref.phoneInput.value.length === 0) {
+                                    this.Ref.phoneLabel.classList.remove('input_label_valid');
+                                }
+
+                                this.init();
+                            }}
+                            oninput={() => {
+                                this.Static.phone = this.Ref.phoneInput.value;
+                                this.init();
+                            }}
+                        />
+                        <label class="input_label" for="phone" ref="phoneLabel">Номер телефона</label>
+                    </div>
+                    <div class="input">
+                        <input
+                            class="input_field"
+                            type="email"
+                            name="email"
+                            autocomplete="off"
+                            id="email"
+                            ref="emailInput"
+                            onchange={() => {
+                                if (this.Ref.emailInput.value.length > 0) {
+                                    this.Ref.emailLabel.classList.add('input_label_valid');
+                                    this.Static.email = this.Ref.emailInput.value;
+                                }
+                                if (this.Ref.emailInput.value.length === 0) {
+                                    this.Ref.emailLabel.classList.remove('input_label_valid');
+                                }
+
+                                this.init();
+                            }}
+                            oninput={() => {
+                                this.Static.email = this.Ref.emailInput.value;
+                                this.init();
+                            }}
+                        />
+                        <label class="input_label" for="email" ref="emailLabel">Email</label>
+                    </div>
+                </div>
+
+                {
+                    this.Static.company ?
+                        <div>
+                            <h3 class="join_title form_title">Представляемая компания</h3>
+                            <div class="form_block">
+                                <div class="input">
+                                    <span></span>
+                                    <input
+                                        class="input_field"
+                                        type="text"
+                                        autocomplete="off"
+                                        id="companyName"
+                                        ref="companyNameInput"
+                                        onchange={() => {
+                                            if (this.Ref.companyNameInput.value.length > 0) {
+                                                this.Ref.companyNameLabel.classList.add('input_label_valid');
+                                                this.Static.companyName = this.Ref.companyNameInput.value
+                                            }
+                                            if (this.Ref.companyNameInput.value.length === 0) {
+                                                this.Ref.companyNameLabel.classList.remove('input_label_valid');
+                                            }
+                                        }}
+                                    />
+                                    <label class="input_label" for="companyName" ref="companyNameLabel">Название</label>
+                                </div>
+                                <div class="input">
+                                    <span></span>
+                                    <input
+                                        class="input_field"
+                                        type="text"
+                                        autocomplete="off"
+                                        id="companyPost"
+                                        ref="companyPostInput"
+                                        onchange={() => {
+                                            if (this.Ref.companyPostInput.value.length > 0) {
+                                                this.Ref.companyPostLabel.classList.add('input_label_valid');
+                                                this.Static.companyPost = this.Ref.companyPostInput.value
+                                            }
+                                            if (this.Ref.companyPostInput.value.length === 0) {
+                                                this.Ref.companyPostLabel.classList.remove('input_label_valid');
+                                            }
+                                        }}
+                                    />
+                                    <label class="input_label" for="companyPost" ref="companyPostLabel">Должность</label>
+                                </div>
+                            </div>
+                        </div> : null
+                }
+
+
             </section>
+
+            <div class="form_btn">
+                <button
+                    class="btn_link btn_link_dark"
+                    onclick={async () => {
+                        let formData = {
+                            company: this.Static.company,
+                            lastName: this.Static.lastName,
+                            firstName: this.Static.firstName,
+                            middleName: this.Static.middleName,
+                            phone: this.Static.phone,
+                            email: this.Static.email,
+                            nameCompany: this.Static.companyName,
+                            postCompany: this.Static.companyPost
+                        };
+
+                        const response = await fetch("http://127.0.0.1/api", {
+                            method: "POST",
+                            body: JSON.stringify(formData)
+                        });
+                        if (!response.ok) {
+                            throw new Error(`Ошибка по адресу , статус ошибки ${response.status}`);
+                        }
+                        let result = await response.json()
+                        console.log('=4b9098=', result)
+                    }}
+                >
+                    Отправить заявку
+                </button>
+            </div>
 
 
         </div>
