15c4497f87dc6b1befb631ab3721ad15b1d3e226
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-09-28 13:21:46 +0300

    from check finish

--- frontends/form/functions.ts
+++ frontends/form/functions.ts
@@ -2,9 +2,11 @@ export const validPhone = function (phone: string) {
   if (!this.Static.regPhone.test(phone)) {
     this.Ref.statusPhone.style.color = "#CF0000";
     this.Ref.statusPhone.innerText = "Неправильно введён номер телефона!"
+    return false
   } else {
     this.Ref.statusPhone.style.color = "#5FAC09";
     this.Ref.statusPhone.innerText = "Номер телефона введён корректно!"
+    return true
   }
 }
 
@@ -24,17 +26,19 @@ export const validText = function (text: string, message: HTMLElement) {
   if (!this.Static.regLetters.test(text)) {
     message.style.color = "#CF0000"
     message.innerText = "Некорректно введены данные!"
+    return false
   } else {
     message.style.color = "#5FAC09"
     message.innerText = "Данные введены корректно!"
+    return true
   }
 }
 
 export const validForm = function (result: any) {
   this.Ref.buttonPreloader.classList.add('preloader-floating-circles')
 
-
   if (this.Static.company) {
+    // юридическое лицо
     if (!this.Static.lastName) {
       setTimeout(() => {
         this.Fn.initOne({
@@ -80,7 +84,7 @@ export const validForm = function (result: any) {
       return
     }
 
-    if (!this.Static.phone) {
+    if (!this.Static.phone && !validPhone(this.Static.phone)) {
       setTimeout(() => {
         this.Fn.initOne({
           name: "modalSuccess",
@@ -95,7 +99,7 @@ export const validForm = function (result: any) {
       return
     }
 
-    if (!this.Static.email) {
+    if (!this.Static.email && !validEmail(this.Static.email)) {
       setTimeout(() => {
         this.Fn.initOne({
           name: "modalSuccess",
@@ -150,6 +154,7 @@ export const validForm = function (result: any) {
             className: 'modal_message_done'
           }
         })
+
         this.Ref.buttonPreloader.classList.remove('preloader-floating-circles')
         this.Static.company = false
         this.Static.lastName = ''
@@ -166,14 +171,13 @@ export const validForm = function (result: any) {
         this.Ref.companyNameInput.value = ''
         this.Static.companyPost = ''
         this.Ref.companyPostInput.value = ''
-        // console.log('=5fc1e8=', 'before map')
-        // this.Static.formMessages.map((item) => {
-        //   console.log('=5fc1e8=', 'inner map')
-        //   item.innerText = ''
 
-        // })
+        Array.from(this.Static.formMessages).map((item: any) => {
+          item.innerText = ''
+        })
 
-
+        this.Static.formStatus = true
+        this.init()
       }, 2000)
     } else {
       setTimeout(() => {
@@ -190,35 +194,131 @@ export const validForm = function (result: any) {
       }, 2000)
     }
 
-
-
   } else {
-    console.log('=d79a52=', 'физическое лицо')
+    // физическое лицо
+    if (!this.Static.lastName) {
+      setTimeout(() => {
+        this.Fn.initOne({
+          name: "modalSuccess",
+          data: {
+            message: 'Корректно заполните поле "Фамилия"',
+            img: this.Static.cancel,
+            className: 'modal_message_cancel'
+          }
+        })
+        this.Ref.buttonPreloader.classList.remove('preloader-floating-circles')
+      }, 2000)
+      return
+    }
+
+    if (!this.Static.firstName) {
+      setTimeout(() => {
+        this.Fn.initOne({
+          name: "modalSuccess",
+          data: {
+            message: 'Корректно заполните поле "Имя"',
+            img: this.Static.cancel,
+            className: 'modal_message_cancel'
+          }
+        })
+        this.Ref.buttonPreloader.classList.remove('preloader-floating-circles')
+      }, 2000)
+      return
+    }
+
+    if (!this.Static.middleName) {
+      setTimeout(() => {
+        this.Fn.initOne({
+          name: "modalSuccess",
+          data: {
+            message: 'Корректно заполните поле "Отчество"',
+            img: this.Static.cancel,
+            className: 'modal_message_cancel'
+          }
+        })
+        this.Ref.buttonPreloader.classList.remove('preloader-floating-circles')
+      }, 2000)
+      return
+    }
+
+    if (!this.Static.phone && !validPhone(this.Static.phone)) {
+      setTimeout(() => {
+        this.Fn.initOne({
+          name: "modalSuccess",
+          data: {
+            message: 'Корректно заполните поле "Номер телефона"',
+            img: this.Static.cancel,
+            className: 'modal_message_cancel'
+          }
+        })
+        this.Ref.buttonPreloader.classList.remove('preloader-floating-circles')
+      }, 2000)
+      return
+    }
+
+
+    if (!this.Static.email && !validEmail(this.Static.email)) {
+      setTimeout(() => {
+        this.Fn.initOne({
+          name: "modalSuccess",
+          data: {
+            message: 'Корректно заполните поле "Адрес электронной почты"',
+            img: this.Static.cancel,
+            className: 'modal_message_cancel'
+          }
+        })
+        this.Ref.buttonPreloader.classList.remove('preloader-floating-circles')
+      }, 2000)
+      return
+    }
+
+    if (result.Status == 'ok') {
+      setTimeout(() => {
+        this.Fn.initOne({
+          name: "modalSuccess",
+          data: {
+            message: result.Message,
+            img: this.Static.done,
+            className: 'modal_message_done'
+          }
+        })
+
+        this.Ref.buttonPreloader.classList.remove('preloader-floating-circles')
+        this.Static.company = false
+        this.Static.lastName = ''
+        this.Ref.lastNameInput.value = ''
+        this.Static.firstName = ''
+        this.Ref.firstNameInput.value = ''
+        this.Static.middleName = ''
+        this.Ref.middleNameInput.value = ''
+        this.Static.phone = ''
+        this.Ref.phoneInput.value = ''
+        this.Static.email = ''
+        this.Ref.emailInput.value = ''
+
+        Array.from(this.Static.formMessages).map((item: any) => {
+          item.innerText = ''
+        })
+
+        this.Static.formStatus = true
+        this.init()
+      }, 2000)
+    } else {
+      setTimeout(() => {
+        this.Fn.initOne({
+          name: "modalSuccess",
+          data: {
+            message: result.Message,
+            img: this.Static.cancel,
+            className: 'modal_message_cancel'
+          }
+        })
+        this.Ref.buttonPreloader.classList.remove('preloader-floating-circles')
+
+      }, 2000)
+    }
   }
 
-
-
-
-
-
-
-
-
-
-  // this.Static.company = false
-  // this.Static.lastName = ''
-  // this.Ref.lastNameInput.value = ''
-  // this.Static.firstName = ''
-  // this.Ref.firstNameInput.value = ''
-  // this.Static.middleName = ''
-  // this.Ref.middleNameInput.value = ''
-  // this.Static.phone = ''
-  // this.Ref.phoneInput.value = ''
-  // this.Static.email = ''
-  // this.Ref.emailInput.value = ''
-
-
-
-  this.init()
+  // this.init()
 }
 
